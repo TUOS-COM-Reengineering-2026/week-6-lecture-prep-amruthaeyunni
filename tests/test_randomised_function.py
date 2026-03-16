2f31456cdcfd4a18b9a31bf80a6bb5287c632b94
Amrutha Eyunni <amruthalu@gmail.com>
2026-03-16 11:44:10 +0000

    commented existing test

--- tests/test_randomised_function.py
+++ tests/test_randomised_function.py
@@ -4,9 +4,11 @@ from unittest.mock import patch
 
 class MyTestCase(unittest.TestCase):
 
+    """
     def test_randomised_function(self):
         self.assertEqual('software', randomised_function())  # This will pass or fail randomly
         # TODO: Can we make this test deterministic? (HINT: Mock testing)
+    """
 
     @patch('lecture.is_small')
     def test_randomised_function_with_mock1(self, mock_is_small):
