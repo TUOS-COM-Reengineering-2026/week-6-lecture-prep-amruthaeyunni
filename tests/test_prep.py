b12aa480e065c7a0b962092e7f219e4af8342680
Amrutha Eyunni <amruthalu@gmail.com>
2026-03-16 10:31:50 +0000

    Added strange function tests to reach 100% coverage

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -9,3 +9,48 @@ class MyTestCase(unittest.TestCase):
         )
 
     # TODO: Can you write more test cases below to increase the test coverage of `strange_function`?
+    def test_strange_function2(self):
+        # a == b and c < d
+        self.assertEqual(
+            first=strange_function(1, 1, 3, 4),
+            second="behaviour 1"
+    )
+        
+    def test_strange_function3(self):
+        # a == b and c >= d
+        self.assertEqual(
+            first=strange_function(1, 1, 4, 3),
+            second="behaviour 2"
+    )
+    
+    def test_strange_function4(self):
+        # a < c
+        self.assertEqual(
+            first=strange_function(1, 5, 3, 4),
+            second="behaviour 3"
+    )
+        
+    def test_strange_function5(self):
+        # a != b and a >= c and d < b
+        self.assertEqual(
+            first=strange_function(5, 7, 3, 4),
+            second="behaviour 4"
+    )
+        
+    def test_strange_function6(self):
+        # a != b and a >= c and d >= b and c < a
+        self.assertEqual(
+            first=strange_function(5, 7, 3, 10),
+            second="behaviour 5"
+    )
+        
+    def test_strange_function7(self):
+        # a != b and a >= c and d >= b and c >= a
+        self.assertEqual(
+            first=strange_function(5, 7, 5, 10),
+            second="behaviour 6"
+    )
+        
+    
+        
+    
